ac1001c1da33866c975ea50cb515746b9b53c4b4
Rwatana <watawata.ryo1231@gmail.com>
2022-10-16 22:28:29 +0900

    1351_count_negative_numbers_in_a_sorted_matrix.py

--- 1351_count_negative_numbers_in_a_sorted_matrix.py
+++ 1351_count_negative_numbers_in_a_sorted_matrix.py
@@ -4,19 +4,16 @@ from typing import List
 class Solution:
     
     def countNegatives(self, grid: List[List[int]]) -> int:
-        yoko = len(grid[0])
-        tate = len(grid)
-
+        col = len(grid[0])
+        row = len(grid)
         i = 0
-        j = yoko -1 
+        j = col -1 
         count = 0
-        while i < tate and j >= 0:
+        while i < row and j >= 0:
             if grid[i][j] < 0:
-                count = count + 1
-
-                j -= 1
-                
+                count += 1
+                j -= 1                
             else:
-                 j = yoko -1
+                 j = col -1
                  i += 1
         return count
