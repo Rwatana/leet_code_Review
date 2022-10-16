e8a167935b2a5b2afaa2439bb213814652b14836
Rwatana <watawata.ryo1231@gmail.com>
2022-10-16 16:36:19 +0900

    744_find_smallest_letter.py

---
+++ 744_find_smallest_letter.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        letters.sort()
+        for i in range(len(letters)):
+            if letters[i] == target:
+                i += 1
+ 
+            elif letters[i] >  target:
+                return letters[i]
+        return letters[0]
