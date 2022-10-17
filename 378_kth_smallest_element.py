33d9ecb5b5da39d9f33dc5f0c40294938dab0db7
Rwatana <watawata.ryo1231@gmail.com>
2022-10-17 15:04:34 +0900

    378_kth_smallest_element.py

---
+++ 378_kth_smallest_element.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        row = len(matrix[0])
+        col = len(matrix)
+        num = []
+        for i in range(row):
+            for j in range(col):
+                num.append(matrix[i][j])
+        num.sort()
+
+        return num[k-1]
