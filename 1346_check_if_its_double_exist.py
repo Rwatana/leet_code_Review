ecc9f751cd072ac77cf512b44b6e350d66358973
Rwatana <114269969+Rwatana@users.noreply.github.com>
2022-10-20 23:46:26 +0900

    Update 1346_check_if_its_double_exist.py

--- 1346_check_if_its_double_exist.py
+++ 1346_check_if_its_double_exist.py
@@ -3,14 +3,16 @@ from typing import List
 
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
+        
         arr.sort()
         for i in range(len(arr)):
-            for j in range(i+1,len(arr),1):
-                if arr[i] <0 and arr[j] < 0:
+            for j in range(i+1,len(arr)):
+                if arr[i] < 0 and arr[j] < 0:
                     i_abs = abs(arr[i])
                     j_abs = abs(arr[j])
-                    if j_abs*2 == i_abs:
+                    if j_abs * 2 == i_abs:
                         return True
+                    
                 if arr[i]*2 == arr[j]:
                         return True
 
