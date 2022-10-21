43722b9566923288b61153fd4b9ff43d7c90b850
Rwatana <watawata.ryo1231@gmail.com>
2022-10-21 15:12:48 +0900

    242_valid_anagram.py

---
+++ 242_valid_anagram.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_list = list(s)
+        t_list = list(t)
+        s_list.sort()
+        t_list.sort()
+        if s_list == t_list:
+            return True
+        
+        return False
