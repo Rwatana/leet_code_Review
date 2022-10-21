e36644b52e60a1176acc83304194a45cbab7524d
Rwatana <watawata.ryo1231@gmail.com>
2022-10-21 21:38:39 +0900

    744_find_smallest_letter.py

--- 744_find_smallest_letter.py
+++ 744_find_smallest_letter.py
@@ -3,10 +3,15 @@ from typing import List
 
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        for i in range(len(letters)):
-            if letters[i] == target:
-                i += 1
- 
-            elif letters[i] >  target:
-                return letters[i]
-        return letters[0]
+        
+        if letters[-1] <= target or letters[0] > target:
+            return letters[0] 
+
+        left , right = 0, len(letters)-1        
+        while left <= right:
+            middle = (right+left)//2
+            if letters[middle] > target:
+                right = middle-1             
+            elif letters[middle] <=  target:
+                left = middle+1
+        return letters[left] 
