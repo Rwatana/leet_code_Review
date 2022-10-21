17c7d6c0c4c25fec07ac6750e741ed181c8cef8e
Rwatana <114269969+Rwatana@users.noreply.github.com>
2022-10-21 22:17:50 +0900

    Update 268_missing_number.py
    I tried the different way. Please check

--- 268_missing_number.py
+++ 268_missing_number.py
@@ -1,15 +1,8 @@
 from typing import List
 
-
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        num= len(nums)
-        lost = 0
-        for i in range (0,num,1):
-            if nums[i]  == nums[0]+i:
-                continue
-            else:
-                lost = nums[0]+i
-                return lost
-        
+            
+        for i in range(0, len(nums) + 1):
+            if i not in nums:
+                return i        
