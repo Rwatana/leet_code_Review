ff85f204fccd9bd1a664e63bfa4f80ee388bb44b
Rwatana <watawata.ryo1231@gmail.com>
2022-10-26 16:47:30 +0900

    add 1-Two-Sum

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -4,23 +4,22 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         n_num = sorted(nums)
-        left , right = 0 , len(nums)-1
+        left, right = 0, len(nums) - 1
         answer_list = []
-        while left < right :
+        while left < right:
             
-            sum = n_num[left]+n_num[right]
+            sum = n_num[left] + n_num[right]
             if sum == target:
                 break
             elif sum > target:
-                right -=1
+                right -= 1
             elif sum < target:
-                left +=1
-
-        for i in range( len(nums ) ):
+                left += 1
+        for i in range(len(nums)):
             if nums[i] == n_num[left]:
                 answer_list.append(i)
                 break
-        for i in range( len(nums ) ):
+        for i in range(len(nums)):
             if nums[i] == n_num[right] and i != answer_list[0]:
                 answer_list.append(i)
                 break
