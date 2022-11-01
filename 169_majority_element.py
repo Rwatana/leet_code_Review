c2490af74ca4fa8915908db16a075c57e934919f
Rwatana <watawata.ryo1231@gmail.com>
2022-11-01 13:10:32 +0900

    Update 169_majority_element.py

--- 169_majority_element.py
+++ 169_majority_element.py
@@ -3,12 +3,10 @@ from typing import List
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        
         nums.sort()
-        length = len(nums)//2
+        length = len(nums) // 2
         if length == 0:
             return nums[0]
-        
-        for i in range(len(nums)-1):
-            if nums[i] == nums[i+length]:
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + length]:
                 return nums[i]
