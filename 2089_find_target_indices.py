e8fc605a0a13b488519b3f4093372479f86dc849
Rwatana <watawata.ryo1231@gmail.com>
2022-11-01 12:22:29 +0900

    Update 2089_find_target_indices.py

--- 2089_find_target_indices.py
+++ 2089_find_target_indices.py
@@ -2,11 +2,11 @@ from typing import List
 
 
 class Solution:
-
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
         nums.sort()
         index = []
         for i, num in enumerate(nums):
             if num == target:
-                index.append(i)                
+                index.append(i)
+                         
         return index
