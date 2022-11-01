7f0850260f7ed279880a6a9e18a2911d22d67977
Rwatana <watawata.ryo1231@gmail.com>
2022-11-01 12:55:23 +0900

    Update 268_missing_number.py

--- 268_missing_number.py
+++ 268_missing_number.py
@@ -1,8 +1,9 @@
 from typing import List
 
+
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
             
         for i in range(0, len(nums) + 1):
             if i not in nums:
-                return i        
+                return i
