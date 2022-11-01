e13c291291d6602a4657b420ba4cdd28b20ac6b4
Rwatana <watawata.ryo1231@gmail.com>
2022-11-01 12:54:05 +0900

    Update 278_first_bad_version.py

--- 278_first_bad_version.py
+++ 278_first_bad_version.py
@@ -1,18 +1,11 @@
-from typing import List
-
-
-# The isBadVersion API is already defined for you.
-# def isBadVersion(version: int) -> bool:
-
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        
-        up , down = 1, n
+        up, down = 1, n
         middle = 0
         while up < down:
-            middle = (up+down)//2
-            if  isBadVersion(middle) == True:
+            middle = (up + down) // 2
+            if isBadVersion(middle) is True:
                 down = middle
             else:
-                up = middle+1                
+                up = middle + 1
         return up
