6f0e05ccd52371c2a5f083f94d7fb6715fb81588
Rwatana <watawata.ryo1231@gmail.com>
2022-11-02 15:45:09 +0900

    Update 167_two_sum.py

--- 167_two_sum.py
+++ 167_two_sum.py
@@ -18,3 +18,17 @@ class Solution:
             elif sum > target:
                 right -= 1
         return 0
+    
+    class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for i in range(len(numbers)):
+            new_target= target-numbers[i]
+            left , right = i+1, len(numbers)-1
+            while left <= right:
+                middle = (left+right)//2
+                if numbers[middle] > new_target:
+                    right = middle-1
+                elif numbers[middle] < new_target:
+                    left = middle+1
+                else:
+                    return [i+1,middle+1]
