989542e49058bfedbef4a5451fffe7c21ef18bcf
Rwatana <watawata.ryo1231@gmail.com>
2022-11-02 13:26:44 +0900

    Update 1346_check_if_its_double_exist.py

--- 1346_check_if_its_double_exist.py
+++ 1346_check_if_its_double_exist.py
@@ -1,17 +1,16 @@
+# T(n) O(1)
 from typing import List
 
 
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        
-        s = set(arr)
-                
-        SS = s - {0}
+        num_set = set(arr)
+        num_set_number = num_set - {0}
         if arr.count(0) > 1:
             return True
 
-        for i in s:
-            if 2 * i in SS:
+        for i in num_set:
+            if 2 * i in num_set_number:
                 return True
 
         return False
