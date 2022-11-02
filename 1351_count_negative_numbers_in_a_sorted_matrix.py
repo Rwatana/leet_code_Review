aa66bde8ae2950371c8452c86f04256773c6b8e1
Rwatana <watawata.ryo1231@gmail.com>
2022-11-02 13:48:16 +0900

    Update 1351_count_negative_numbers_in_a_sorted_matrix.py

--- 1351_count_negative_numbers_in_a_sorted_matrix.py
+++ 1351_count_negative_numbers_in_a_sorted_matrix.py
@@ -1,18 +1,16 @@
+# T(n) O(1)
 from typing import List
 
 
 class Solution:
-    def countNegatives(self, grid: List[List[int]]) -> int:
-        col = len(grid[0])
-        row = len(grid)
-        i = 0
-        j = col - 1
-        count = 0
-        while i < row and j >= 0:
-            if grid[i][j] < 0:
-                count += 1
-                j -= 1
-            else:
-                j = col - 1
-                i += 1
-        return count
+    def checkIfExist(self, arr: List[int]) -> bool:
+        num_set = set(arr)
+        num_set_number = num_set - {0}
+        if arr.count(0) > 1:
+            return True
+
+        for i in num_set:
+            if 2 * i in num_set_number:
+                return True
+
+        return False
