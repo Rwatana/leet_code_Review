46ce8bf95b95e4e01b4a3d1f5deb64813abcc0c5
Rwatana <watawata.ryo1231@gmail.com>
2022-11-03 13:07:53 +0900

    Update 2089_find_target_indices.py

--- 2089_find_target_indices.py
+++ 2089_find_target_indices.py
@@ -1,13 +1,43 @@
-# T(n) O(1)
+# Time complexity O(n)
+# Space complexity O(1)
 from typing import List
 
 
 class Solution:
+    
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
         nums.sort()
-        index = []
-        for i, num in enumerate(nums):
-            if num == target:
-                index.append(i)
-                         
-        return index
+        
+        def binarySearchLeft(nums, target):
+            left, right = 0, len(nums) - 1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] < target:
+                    left = mid + 1
+                
+                else:
+                    right = mid - 1
+
+            return left
+
+        def binarySearchRight(nums, target):
+            left, right = 0, len(nums) - 1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] <= target:
+                    left = mid + 1
+                
+                else:
+                    right = mid - 1
+
+            return right
+
+        leftvalue = binarySearchLeft(nums, target)
+        rightvalue = binarySearchRight(nums, target)
+        if leftvalue < rightvalue:
+            return [i for i in range(leftvalue, rightvalue + 1)]
+
+        if leftvalue == rightvalue:
+            return [leftvalue]
+        
+        return []
