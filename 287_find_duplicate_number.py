dbdc7f2932ac142b691561314474ed5e46fbe45d
Rwatana <watawata.ryo1231@gmail.com>
2022-11-03 11:50:42 +0900

    Update 287_find_duplicate_number.py

--- 287_find_duplicate_number.py
+++ 287_find_duplicate_number.py
@@ -1,9 +1,20 @@
+# time complexity O(logn)
+# space complexity O(1)
 from typing import List
 
 
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        nums.sort()
-        for i in range(len(nums)):
-            if nums[i] == nums[i + 1]:
-                return nums[i]
+        n = len(nums) - 1
+        left, right = 1, n
+        while left < right:
+            mid = (right + left) // 2
+            count = 0
+            for num in nums:
+                if num <= mid:
+                    count += 1
+            if count > mid:
+                right = mid
+            else:
+                left = mid + 1
+        return right
