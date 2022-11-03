7189d7e5299c136ae141e76be92fadb5840eb7b6
Rwatana <watawata.ryo1231@gmail.com>
2022-11-03 11:04:42 +0900

    Update 74_search_2d_matrix.py

--- 74_search_2d_matrix.py
+++ 74_search_2d_matrix.py
@@ -1,13 +1,22 @@
+# time complexity O(logn)
+# space complexity O(n)
 from typing import List
 
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix:
+            return False
         row = len(matrix)
         col = len(matrix[0])
-
-        for i in range(row):
-            for j in range(col):
-                if matrix[i][j] == target:
-                    return True
+        left, right = 0, row * col - 1
+        while left <= right:
+            middle = (left + right) // 2
+            value = matrix[middle // col][middle % col]
+            if value == target:
+                return True
+            elif value > target:
+                right = middle - 1
+            else:
+                left = middle + 1
         return False
