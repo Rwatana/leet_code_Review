76f646788a44287a1c2f7be0984109a7d4dcc9d7
Rwatana <watawata.ryo1231@gmail.com>
2022-11-03 09:02:24 +0900

    Update 153_find_minimum.py

--- 153_find_minimum.py
+++ 153_find_minimum.py
@@ -1,10 +1,15 @@
+# time complexity O(n)
+# space complexity O(1)
 from typing import List
 
 
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        i=0
-        for i in range(1,len(nums),1):
-            if nums[-i] < nums[-i-1]:
-                return nums[-i]
-        return nums[0]
+        left, right = 0, len(nums) - 1
+        while left < right:
+            middle = (left + right) // 2
+            if nums[middle] < nums[right]:
+                right = middle
+            else:
+                left = middle + 1
+        return nums[right]
