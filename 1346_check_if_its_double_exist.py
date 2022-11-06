f1b6f420e1a384abff46be9792093093a761d0d7
Rwatana <watawata.ryo1231@gmail.com>
2022-11-06 09:45:26 +0900

    Create 1346_check_if_its_double_exist.py

---
+++ 1346_check_if_its_double_exist.py
@@ -0,0 +1,15 @@
+# time cpmplexity O(n)
+# space complexityO(1)
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        
+        s = set(arr)        
+        SS = s - {0}
+        if arr.count(0) > 1:
+            return True
+
+        for i in s:
+            if 2*i in SS:
+                return True
+
+        return False
