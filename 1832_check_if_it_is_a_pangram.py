e9d6c284d9b6d2c51f73bd43bef29da0e827b1ad
Rwatana <watawata.ryo1231@gmail.com>
2022-11-06 12:31:19 +0900

    Update 1832_check_if_it_is_a_pangram.py

--- 1832_check_if_it_is_a_pangram.py
+++ 1832_check_if_it_is_a_pangram.py
@@ -1,6 +1,6 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
         checker = [0] * 26
-        for i in sentence:
-            checker[ord(i) - ord('a')] += 1
+        for character in sentence:
+            checker[ord(character) - ord('a')] += 1
         return 0 not in checker
