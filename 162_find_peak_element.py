ed7a3e62f754422d05819976c5608194fe5d3065
Rwatana <watawata.ryo1231@gmail.com>
2022-11-06 14:19:28 +0900

    Update 162_find_peak_element.py

--- 162_find_peak_element.py
+++ 162_find_peak_element.py
@@ -9,7 +9,7 @@ class Solution:
         left = 0
         right = len(nums) - 1
         while left < right:
-            mid = (left + right) // 2
+            mid = (left+right) // 2
             if nums[mid] > nums[mid + 1]:
                 right = mid
             else:
