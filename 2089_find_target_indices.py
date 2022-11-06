a6f4efa5c8f89cd2dd1a8256a7feb95b95bd632d
Rwatana <watawata.ryo1231@gmail.com>
2022-11-06 14:59:05 +0900

    Update 2089_find_target_indices.py

--- 2089_find_target_indices.py
+++ 2089_find_target_indices.py
@@ -1,43 +1,25 @@
-# Time complexity O(n)
-# Space complexity O(1)
+# Time complexity O(nlogn)
+# Space complexity O(n)
 from typing import List
 
 
 class Solution:
-    
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
         nums.sort()
-        
-        def binarySearchLeft(nums, target):
-            left, right = 0, len(nums) - 1
-            while left <= right:
-                mid = (left + right) // 2
-                if nums[mid] < target:
-                    left = mid + 1
-                
-                else:
-                    right = mid - 1
-
-            return left
-
-        def binarySearchRight(nums, target):
-            left, right = 0, len(nums) - 1
-            while left <= right:
-                mid = (left + right) // 2
-                if nums[mid] <= target:
-                    left = mid + 1
-                
-                else:
-                    right = mid - 1
-
-            return right
-
-        leftvalue = binarySearchLeft(nums, target)
-        rightvalue = binarySearchRight(nums, target)
-        if leftvalue < rightvalue:
-            return [i for i in range(leftvalue, rightvalue + 1)]
-
-        if leftvalue == rightvalue:
-            return [leftvalue]
-        
-        return []
+        left, right = 0, len(nums) - 1
+        answer_array = []
+        while left <= right:
+            middle = (left+right) // 2
+            if nums[middle] > target:
+                right = middle - 1
+            elif nums[middle] < target:
+                left = middle + 1
+            elif nums[middle] == target:
+                if nums[left] < target:
+                    left += 1
+                elif nums[right] > target:
+                    right -= 1
+                if nums[left] == target and nums[right] == target:
+                    answer_array.append(left)
+                    left += 1
+        return answer_array
