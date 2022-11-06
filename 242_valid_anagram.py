274f1d65c12c18cdef63685cea67456cc0fb2ad8
Rwatana <watawata.ryo1231@gmail.com>
2022-11-06 13:36:04 +0900

    Update 242_valid_anagram.py

--- 242_valid_anagram.py
+++ 242_valid_anagram.py
@@ -1,9 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_list = list(s)
-        t_list = list(t)
-        s_list.sort()
-        t_list.sort()
-        if s_list == t_list:
-            return True        
-        return False
+        return sorted(s) == sorted(t)
