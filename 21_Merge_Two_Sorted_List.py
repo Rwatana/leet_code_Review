47c145ebe169b82b40061d160f9e26b3ef492bd2
Rwatana <watawata.ryo1231@gmail.com>
2022-11-07 10:22:40 +0900

    Update 21_Merge_Two_Sorted_List.py

--- 21_Merge_Two_Sorted_List.py
+++ 21_Merge_Two_Sorted_List.py
@@ -11,18 +11,18 @@ from typing import Optional
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        new_Head = dummy_Head = ListNode()
+        new_head = dummy_head = ListNode()
         while list1 and list2:
             if list1.val < list2.val:
-                dummy_Head.next = list1
+                dummy_head.next = list1
                 list1 = list1.next
             else:
-                dummy_Head.next = list2
+                dummy_head.next = list2
                 list2 = list2.next
-            dummy_Head = dummy_Head.next
+            dummy_head = dummy_head.next
         
         if list1:
-            dummy_Head.next = list1
+            dummy_head.next = list1
         if list2:
-            dummy_Head.next = list2
-        return new_Head.next
+            dummy_head.next = list2
+        return new_head.next
