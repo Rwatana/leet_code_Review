7a2dd48f7787674cba4fba6e9eb9f6dc9bcdc38e
Rwatana <watawata.ryo1231@gmail.com>
2022-11-13 01:41:07 +0900

    Update 1_two_sum.py

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -1,13 +1,16 @@
 # time: O(n)
-# space: O(1)
+# space: O(n)
 from typing import List
 
 
 class Solution:
+    
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         from_num_to_supplement = {}
         for i, num in enumerate(nums):
             if target - num in from_num_to_supplement:
                 return [from_num_to_supplement[target - num], i]
+            
             from_num_to_supplement[num] = i
+            
             return []
