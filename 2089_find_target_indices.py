f8919282acfe69498a3aa26b191a2c3a7cb4794d
Rwatana <watawata.ryo1231@gmail.com>
2022-11-13 01:06:36 +0900

    Update 2089_find_target_indices.py

--- 2089_find_target_indices.py
+++ 2089_find_target_indices.py
@@ -1,25 +1,24 @@
+# n = len(nums)
 # Time complexity O(nlogn)
 # Space complexity O(n)
 from typing import List
 
 
 class Solution:
+    
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
         nums.sort()
         left, right = 0, len(nums) - 1
-        answer_array = []
-        while left <= right:
-            middle = (left+right) // 2
-            if nums[middle] > target:
-                right = middle - 1
-            elif nums[middle] < target:
-                left = middle + 1
-            elif nums[middle] == target:
-                if nums[left] < target:
-                    left += 1
-                elif nums[right] > target:
-                    right -= 1
-                if nums[left] == target and nums[right] == target:
-                    answer_array.append(left)
-                    left += 1
-        return answer_array
+        target_indices = []        
+        while 1 < right - left:
+            mid = (left+right) // 2
+            if nums[mid] < target:
+                left = mid
+            else:
+                right = mid
+
+        while right < len(nums) and nums[right] == target:
+            target_indices.append(right)
+            right += 1
+            
+        return target_indices
