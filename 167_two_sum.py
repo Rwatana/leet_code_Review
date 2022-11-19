b43c5462015761c3824b12d7784671fe6e9db470
Rwatana <watawata.ryo1231@gmail.com>
2022-11-19 12:32:47 +0900

    Update 167_two_sum.py

--- 167_two_sum.py
+++ 167_two_sum.py
@@ -1,34 +1,18 @@
+# time:(nlogn)
+# space: (1)
 from typing import List
 
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        left = 0
-        right = len(numbers) - 1
-        indices = []
-        i = 0
-        while i < 1:
-            sum = numbers[left] + numbers[right]
-            if sum == target:
-                indices.append(left + 1)
-                indices.append(right + 1)
-                return indices
-            elif sum < target:
-                left += 1
-            elif sum > target:
-                right -= 1
-        return 0
-    
-    class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
         for i in range(len(numbers)):
-            new_target= target-numbers[i]
-            left , right = i+1, len(numbers)-1
+            new_target = target - numbers[i]
+            left, right = i + 1, len(numbers) - 1
             while left <= right:
-                middle = (left+right)//2
+                middle = (left+right) // 2
                 if numbers[middle] > new_target:
-                    right = middle-1
+                    right = middle - 1
                 elif numbers[middle] < new_target:
-                    left = middle+1
+                    left = middle + 1
                 else:
-                    return [i+1,middle+1]
+                    return [i + 1, middle + 1]
