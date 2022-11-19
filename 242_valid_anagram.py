298b49e6d3ac603770df2b018ba18b90ea84c517
Rwatana <watawata.ryo1231@gmail.com>
2022-11-19 13:46:39 +0900

    Update 242_valid_anagram.py

--- 242_valid_anagram.py
+++ 242_valid_anagram.py
@@ -2,13 +2,16 @@
 # space: O(n)
 
 class Solution:
+    
     def isAnagram(self, s: str, t: str) -> bool:
         char_freq = Counter(s)
         for char in t:
             if not char in char_freq:
                 return False
+            
             else:
                 char_freq[char] -= 1
                 if char_freq[char] == 0:
                     del char_freq[char]
+                    
         return True if not char_freq else False
