a95d04e019dafc18af4943c11f7c6d488ae2a822
Rwatana <watawata.ryo1231@gmail.com>
2022-11-19 13:57:39 +0900

    Update 922_sort_array_by_parity_2.py

--- 922_sort_array_by_parity_2.py
+++ 922_sort_array_by_parity_2.py
@@ -1,13 +1,13 @@
 # time: O(n)
-# space: O(n)
+# space: O(1)
 from typing import List
 
 
 class Solution:
     
     def sortArrayByParityII(self, nums: List[int]) -> List[int]:
-        even, odd, full = 0, 1, len(nums) - 1
-        while odd <= full:
+        even, odd, nums_length = 0, 1, len(nums) - 1
+        while odd <= nums_length:
             if nums[odd] % 2 == 1:
                 odd += 2
             else:
