17d8c3bea25c19d1f85d61199cbc7342041a16af
Rwatana <watawata.ryo1231@gmail.com>
2022-11-19 14:55:32 +0900

    Update 977_squares_of_sorted_array.py

--- 977_squares_of_sorted_array.py
+++ 977_squares_of_sorted_array.py
@@ -1,9 +1,22 @@
-# time: O(1)
-# space: O(1)
+# time: O(n)
+# space: O(n)
 from typing import List
 
 
 class Solution:
     
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        return sorted([x**2 for x in nums])
+        double_list = []
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            left_double = (nums[left])**2
+            right_double = (nums[right])**2
+            if left_double > right_double:
+                double_list.append(left_double)
+                left += 1
+            else:
+                double_list.append(right_double)
+                right -= 1
+        double_list.append((nums[left])**2)
+        return double_list[::-1]
