af4733e55b8d79143a3740476575ef40e17ca397
Rwatana <watawata.ryo1231@gmail.com>
2022-11-19 15:25:47 +0900

    Update 1_two_sum.py

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -7,6 +7,7 @@ class Solution:
     
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         from_num_to_supplement = {}
+        
         for i, num in enumerate(nums):
             if target - num in from_num_to_supplement:
                 return [from_num_to_supplement[target - num], i]
