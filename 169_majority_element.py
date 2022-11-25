a61218d1790fe98c9d6294a49f734aac19490deb
Rwatana <watawata.ryo1231@gmail.com>
2022-11-25 12:15:47 +0900

    Update 169_majority_element.py

--- 169_majority_element.py
+++ 169_majority_element.py
@@ -1,9 +1,17 @@
-# time: O(1)
+# time: O(n)
 # space: O(1)
 from typing import List
 
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums) // 2]
+        count = 0
+        candidate_of_majority_element = None
+        for num in nums:
+            if count == 0:
+                candidate_of_majority_element = num
+            if num == candidate_of_majority_element:
+                count += 1
+            else:
+                count -= 1
+        return candidate_of_majority_element
