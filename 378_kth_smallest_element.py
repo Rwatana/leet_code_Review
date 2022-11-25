c29cf4e322ccd08cc8814d8498adf5c009b17ef2
Rwatana <watawata.ryo1231@gmail.com>
2022-11-25 12:03:22 +0900

    Update 378_kth_smallest_element.py

--- 378_kth_smallest_element.py
+++ 378_kth_smallest_element.py
@@ -1,13 +1,30 @@
+# n = matrix.length (= matrix[i].length)
+# m = matrix[-1][-1] - (matrix[0][0] - 1)
+# time: n*logn*logm
+# space: O(1)
+import bisect
 from typing import List
 
 
 class Solution:
+    
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        row = len(matrix[0])
-        col = len(matrix)
-        num = []
-        for i in range(row):
-            for j in range(col):
-                num.append(matrix[i][j])
-        num.sort()
-        return num[k - 1]
+        if not matrix:
+            raise ValueError("matrix should not be empty.")
+        
+        if k is None:
+            raise ValueError("k should not be empty.")
+        
+        for row in matrix:
+            if len(matrix) != len(row):
+                raise ValueError("The number of rows and columns should be the same.")
+            
+        left = matrix[0][0] - 1
+        right = matrix[-1][-1]
+        while right - left > 1:
+            mid = (left+right) // 2
+            if sum(bisect.bisect(row, mid) for row in matrix) < k:
+                left = mid
+            else:
+                right = mid
+        return right
