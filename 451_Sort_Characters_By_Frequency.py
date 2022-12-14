40b9034ecb58a4b0d665568f8bf2009a2a946f0c
watanabe ryoma <ryoma@watanabenoMacBook-Air.local>
2022-12-14 11:13:14 +0900

    451_Sort_Characters_By_Frequency.py

---
+++ 451_Sort_Characters_By_Frequency.py
@@ -0,0 +1,17 @@
+# T(n)
+# S(n)
+from typing import collections
+
+
+class Solution:
+    def frequencySort(self, s: str) -> str:
+
+        cnt = collections.defaultdict(int)
+        for c in s:
+            cnt[c] += 1
+        
+        # Sort and Build string
+        res = []
+        for k, v in sorted(cnt.items(), key=lambda x: -x[1]):
+            res += [k] * v
+        return "".join(res)
