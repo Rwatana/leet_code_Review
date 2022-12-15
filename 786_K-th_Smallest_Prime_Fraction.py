746f66bea19761fa5cad0973a02dad9099c6f2de
watanabe ryoma <ryoma@watanabenoMacBook-Air.local>
2022-12-15 15:07:25 +0900

    786_K-th_Smallest_Prime_Fraction.py

--- 786_K-th_Smallest_Prime_Fraction.py
+++ 786_K-th_Smallest_Prime_Fraction.py
@@ -1,5 +1,5 @@
-# T(n**2)
-# S(n)
+# Time: O(n**2)
+# Space: O(n)
 from typing import List
 
 
@@ -7,25 +7,25 @@ class Solution:
 
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
         lo, hi = 0, 1.0
-        n = len(arr)
+        arr_length = len(arr)
         while lo < hi:
-            m = (lo+hi) / 2
-            maxf, p, q, cnt = 0.0, 0, 0, 0
-            j=0
-            for i in range(n - 1):
-                while j < n and arr[i] / arr[j] > m:
+            mid = (lo+hi) / 2
+            maxf, i_1, i_2, cnt = 0.0, 0, 0, 0
+            j = 0
+            for i in range(arr_length - 1):
+                while j < arr_length and arr[i] / arr[j] > mid:
                     j += 1
-                cnt += n - j
-                if j == n:
+                cnt += arr_length - j
+                if j == arr_length:
                     break
 
                 if arr[i] / arr[j] > maxf:
                     maxf = arr[i] / arr[j]
-                    p, q = i, j
+                    i_1, i_2 = i, j
 
             if cnt > k:
-                hi = m
+                hi = mid
             elif cnt < k:
-                lo = m
+                lo = mid
             else:
-                return [arr[p], arr[q]]
+                return [arr[i_1], arr[i_2]]
