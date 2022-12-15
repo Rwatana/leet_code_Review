363fe8a176ed9b26ffbca86b08146a9b770884d0
watanabe ryoma <ryoma@watanabenoMacBook-Air.local>
2022-12-16 07:26:34 +0900

    449_Serialize_and_Deserialize_BST.py

---
+++ 449_Serialize_and_Deserialize_BST.py
@@ -0,0 +1,31 @@
+# Time: O(n)
+# Space: O(n)
+
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Codec:
+
+    def serialize(self, root: TreeNode) -> str:
+        if not root:
+            return ''
+        return str(root.val) + ',' + self.serialize(root.left)
+        + self.serialize(root.right)
+        
+    def deserialize(self, data: str) -> TreeNode:
+        deq = deque(int(val) for val in data.split(',') if val)
+        
+        def build_tree(lower_bound, upper_bound):
+            if deq and lower_bound < deq[0] < upper_bound:
+                val = deq.popleft()
+                return TreeNode(val, build_tree(lower_bound, val),
+                                build_tree(val, upper_bound))
+               
+        return build_tree(float('-inf'), float('inf'))
