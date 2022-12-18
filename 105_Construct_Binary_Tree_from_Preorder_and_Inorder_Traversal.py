ea1877707e4aff51eb409ee766bd1349055127eb
watanabe ryoma <ryoma@watanabenoMacBook-Air.local>
2022-12-18 16:05:39 +0900

    105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.py

---
+++ 105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.py
@@ -0,0 +1,20 @@
+# time: O(logn)
+# space: O(1)
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+        
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if inorder:
+            ind = inorder.index(preorder.pop(0))
+            root = TreeNode(inorder[ind])
+            root.left = self.buildTree(preorder, inorder[0:ind])
+            root.right = self.buildTree(preorder, inorder[ind + 1:])
+            return root
