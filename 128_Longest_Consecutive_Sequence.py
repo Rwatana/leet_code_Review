28fb85a7a687bd639f68309f8546b7aa3a5106cc
watanabe ryoma <ryoma@watanabenoMacBook-Air.local>
2022-12-18 15:36:34 +0900

    128_Longest_Consecutive_Sequence.py

---
+++ 128_Longest_Consecutive_Sequence.py
@@ -0,0 +1,27 @@
+# TIme: O()
+# Space: O()
+from typing import List
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        concecutive = {}
+        res = 0
+        for k in nums:
+            if k in concecutive:
+                continue
+            concecutive[k] = [k, k]
+            
+            if k - 1 in concecutive:
+                lst = concecutive[k - 1]
+                lst[1] = concecutive[k][1]
+                concecutive[k] = concecutive[lst[1]] = lst
+                
+            if k + 1 in concecutive:
+                lst = concecutive[k + 1]
+                lst[0] = concecutive[k][0]
+                concecutive[k] = concecutive[lst[0]] = lst
+                
+            left, right = concecutive[k]
+            res = max(res, right - left + 1)
+        return res
