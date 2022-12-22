cc6ba6096ffa2c8be9fa98cc2af5a4b59bf50e2d
watanabe ryoma <ryoma@watanabenoMacBook-Air.local>
2022-12-22 16:24:00 +0900

    442_Find_All_Duplicates_in_an_Array.py

---
+++ 442_Find_All_Duplicates_in_an_Array.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        freq, result = [0] * (len(nums)+1), []
+        for num in nums:
+            if freq[num] == 1:
+                result.append(num)
+            else:
+                freq[num] = 1
+        return result
