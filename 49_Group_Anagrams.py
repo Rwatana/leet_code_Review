523717e667f21c0ae57e88fb71a0df96a153ed1d
watanabe ryoma <ryoma@watanabenoMacBook-Air.local>
2022-12-22 16:34:34 +0900

    49_Group_Anagrams.py

---
+++ 49_Group_Anagrams.py
@@ -0,0 +1,12 @@
+from typing import List
+import collections
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram = collections.defaultdict(list)
+        
+        for i in range(len(strs)):
+            anagram[''.join(sorted(strs[i]))].append(strs[i])
+            
+        return list(anagram.values())
