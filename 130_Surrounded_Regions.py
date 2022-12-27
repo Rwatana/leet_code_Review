1843d6034c26ee6f8cdd88428ed7ebec6b04689f
watanabe ryoma <ryoma@watanabenoMacBook-Air.local>
2022-12-27 20:05:36 +0900

    130_Surrounded_Regions.py

---
+++ 130_Surrounded_Regions.py
@@ -0,0 +1,28 @@
+# Time: O(n**2)
+# Space: O(1)
+class Solution(object):
+
+    def solve1(self, board):
+        if not board or not board[0]:
+            return
+        for i in [0, len(board) - 1]:
+            for j in range(len(board[0])):
+                self.dfs(board, i, j)
+        for i in range(len(board)):
+            for j in [0, len(board[0]) - 1]:
+                self.dfs(board, i, j)
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                elif board[i][j] == '.':
+                    board[i][j] = 'O'
+                
+    def dfs(self, board, i, j):
+        
+        if 0 <= i < len(board) and 0 <= j < len(board[0]) and board[i][j] == 'O':
+            board[i][j] = '.'
+            self.dfs(board, i + 1, j)
+            self.dfs(board, i - 1, j)
+            self.dfs(board, i, j + 1)
+            self.dfs(board, i, j - 1)
