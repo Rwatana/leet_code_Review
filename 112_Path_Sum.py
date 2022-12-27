9ff65396e91767ec0735f738a4366cf5efc6aeed
watanabe ryoma <ryoma@watanabenoMacBook-Air.local>
2022-12-27 19:45:31 +0900

    112_Path_Sum.py

---
+++ 112_Path_Sum.py
@@ -0,0 +1,30 @@
+# Time: O(logn)
+# Space: O(n)
+class TreeNode:
+    
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        
+        if not root:
+            return False
+        
+        stack = []
+        
+        stack.append((root, sum - root.val))
+        
+        while stack:
+            node, sum = stack.pop()
+            if not node.left and not node.right and sum == 0:
+                return True
+            if node.left:
+                stack.append((node.left, sum - node.left.val))
+            if node.right:
+                stack.append((node.right, sum - node.right.val))
+        return False
