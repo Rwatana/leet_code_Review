74c8f2d8341420385a9367988194ce86c001ef48
watanabe ryoma <ryoma@ryomamac.local>
2023-08-28 17:58:47 +0900

    695_Max_Area_of_Island

--- 695_Max_Area_of_Island.py
+++ 695_Max_Area_of_Island.py
@@ -6,20 +6,20 @@ from typing import List
 
 
 class Solution:
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+    def count_the_max_are_of_island(self, grid: List[List[int]]) -> int:
         row, col = len(grid), len(grid[0])
         max_area = 0
 
-        def check(row: int, col: int) -> int:
-            if 0 <= row < len(grid)
-                    and 0 <= col < len(grid[0]) 
-                    and grid[row][col] == 1:
+        def count_the_are_of_same_island(row: int, col: int) -> int:
+            if {0 <= row < len(grid)
+                    and 0 <= col < len(grid[0])
+                    and grid[row][col] == 1}:
                 grid[row][col] = 0
                 area = 1
-                area += check(row + 1, col)
-                area += check(row - 1, col)
-                area += check(row, col + 1)
-                area += check(row, col - 1)
+                area += count_the_are_of_same_island(row + 1, col)
+                area += count_the_are_of_same_island(row - 1, col)
+                area += count_the_are_of_same_island(row, col + 1)
+                area += count_the_are_of_same_island(row, col - 1)
                 return area
 
             return 0
@@ -27,7 +27,7 @@ class Solution:
         for i in range(row):
             for j in range(col):
                 if grid[i][j] == 1:
-                    num_area = check(i, j)
+                    num_area = count_the_are_of_same_island(i, j)
                     max_area = max(max_area, num_area)
         
         return max_area
