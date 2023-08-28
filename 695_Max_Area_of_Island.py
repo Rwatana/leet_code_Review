17240045ee7aaac4689da7d6df4dfd275eff3d61
watanabe ryoma <ryoma@ryomamac.local>
2023-08-28 18:05:16 +0900

    695_Max_Area_of_Island

--- 695_Max_Area_of_Island.py
+++ 695_Max_Area_of_Island.py
@@ -6,20 +6,20 @@ from typing import List
 
 
 class Solution:
-    def count_the_max_are_of_island(self, grid: List[List[int]]) -> int:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         row, col = len(grid), len(grid[0])
         max_area = 0
 
-        def count_the_are_of_same_island(row: int, col: int) -> int:
-            if {0 <= row < len(grid)
-                    and 0 <= col < len(grid[0])
-                    and grid[row][col] == 1}:
-                grid[row][col] = 0
+        def count_the_width_of_island(cordinate_x: int, cordinate_y: int) -> int:
+            if {0 <= cordinate_x < len(grid)
+                    and 0 <= cordinate_y < len(grid[0])
+                    and grid[cordinate_x][cordinate_y] == 1}:
+                grid[cordinate_x][cordinate_y] = 0
                 area = 1
-                area += count_the_are_of_same_island(row + 1, col)
-                area += count_the_are_of_same_island(row - 1, col)
-                area += count_the_are_of_same_island(row, col + 1)
-                area += count_the_are_of_same_island(row, col - 1)
+                area += count_the_width_of_island(cordinate_x + 1, cordinate_y)
+                area += count_the_width_of_island(cordinate_x - 1, cordinate_y)
+                area += count_the_width_of_island(cordinate_x, cordinate_y + 1)
+                area += count_the_width_of_island(cordinate_x, cordinate_y - 1)
                 return area
 
             return 0
@@ -27,7 +27,7 @@ class Solution:
         for i in range(row):
             for j in range(col):
                 if grid[i][j] == 1:
-                    num_area = count_the_are_of_same_island(i, j)
+                    num_area = count_the_width_of_island(i, j)
                     max_area = max(max_area, num_area)
         
         return max_area
